e7ecaf972b912eebf89c3f7188884d7469b78ad6
Ignacio <ignaciopirisb@gmail.com>
2022-04-29 20:39:24 -0300

    trigger sell tokens to detect tax 7

--- contracts/interfaces/ICustomRouter.sol
+++ contracts/interfaces/ICustomRouter.sol
@@ -2,10 +2,15 @@ pragma solidity >=0.6.0 <0.8.0;
 
 interface ICustomRouter {
     function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
+        uint256 amountIn,
+        uint256 amountOutMin,
         address[] calldata path,
         address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function getAmountsOut(uint256 amountIn, address[] calldata path)
+        public
+        view
+        returns (uint256[] memory amounts);
 }
